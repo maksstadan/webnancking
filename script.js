fa01c832434d484f3cf0e93b414ae9ec627b6936
maksstadan <maksstadan@gmail.com>
2024-11-16 12:40:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const ownerKey = "gjnrbirdibnbgdbhntr";
+const url = "https://thread-apricot-index.glitch.me/cards/";
+
+document.querySelector("#new").addEventListener("click", () => {
+    fetch(url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"ownerKey": ownerKey})
+    }).then(async function (response) {
+        console.log(await response.json());
+    })
+})
+
+fetch(url + ownerKey).then(async function (response) {
+    console.log(await response.json());
+    
+})
