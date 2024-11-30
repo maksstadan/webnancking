28c8c662a9444bad53fee01a3d66ace21a0516a4
maksstadan <maksstadan@gmail.com>
2024-11-30 12:18:22 +0200

    add all

--- script.js
+++ script.js
@@ -28,26 +28,69 @@ function render(cards) {
                 <td>${formattedNumber}</td>
                 <td>${card.expireDate}</td>
                 <td>${card.balance}₴</td>
-                <td><input type="password" value="${card.cvv}" readonly></td>
+                <td class="position-relative">
+                    <input type="password" value="${card.cvv}" readonly id="input-${card.cvv}">
+                    <i class="fa-solid fa-eye fa-lg position-absolute top-50 ms-3" id="${card.cvv}" style="cursor: pointer;"></i>
+                </td>
                 <td>
-                    <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <button onclick="setNumber('${card.cardNumber}')" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <i class="fa-solid fa-pen-to-square"></i>
                     </button>
                 </td>
                 <td>
-                    <button class="btn btn-danger" onclick="deleteCard('${card.cardNumber}')">
+                    <button class="btn btn-danger" 
+                    onclick="deleteCard('${card.cardNumber}')">
                         <i class="fa-solid fa-trash"></i>
                     </button>
                 </td>
             <tr>
         `
     }
+    let eye = document.querySelectorAll(".fa-eye");
+    Array.from(eye).forEach(item => item.addEventListener("mouseenter", show));
+    Array.from(eye).forEach(item => item.addEventListener("mouseleave", hide));
 }
+
+function show() {
+    let idInput = "input-" + this.id;
+    document.querySelector(`#${idInput}`).type = "text";
+}
+
+function hide() {
+    let idInput = "input-" + this.id;
+    document.querySelector(`#${idInput}`).type = "password";
+}
+
+let hiddenNumber = document.querySelector("#card-number");
+function setNumber(number) {
+    hiddenNumber.value = number;
+}
+
+let form = document.querySelector("#form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let number = hiddenNumber.value;
+    let amount = event.target["amount"].value;
+    let choice = Array.from(event.target["type"]).find(radio => radio.checked);
+    if(amount < 0) amount = Math.abs(amount);
+    if(choice.id == "withdraw") amount *= -1;
+
+    fetch(url + number + "/balance", {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"amount": amount })
+    }).then(async function (response) {
+        getData();
+    })
+})
 
 function deleteCard(number) {
     fetch(url + number, {
         method: "DELETE"
     }).then(async function (response) {
+        let data = await response.json();
+        if(response.status == 403) return alert(data.error);
+        alert(data.message)
         getData();
     })
 }
